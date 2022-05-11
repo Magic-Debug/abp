42c9fc868500070c8b7eb582a808ef29c29779eb
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2022-05-11 14:59:54 +0800

    signalr-hubs-blogging

--- e-Shop-Blogging/Volo.Blogging.App/PushHub.cs
+++ e-Shop-Blogging/Volo.Blogging.App/PushHub.cs
@@ -11,11 +11,28 @@ namespace Volo.Blogging.App;
 /// /signalr-hubs/push
 /// </summary>
 //[Authorize]
+
+[HubRoute("/signalr-hubs/blogging")]
 public class PushHub : AbpHub
 {
 
-    public PushHub(ILogger<PushHub>  log)
+    public PushHub(ILogger<PushHub> log)
     {
+        log.LogInformation("Push Hub");
+    }
+
+    public async override Task OnConnectedAsync()
+    {
+        Logger.LogInformation("on connected");
+        await base.OnConnectedAsync();
+
+        await Clients.All.SendAsync("welcome", new { ConnectionId = this.Context.ConnectionId, Time = DateTime.Now });
+    }
+
+    public async override Task OnDisconnectedAsync(Exception exception)
+    {
+        await Clients.AllExcept(Context.ConnectionId).SendAsync("onDisconnected", new { ConnectionId = Context.ConnectionId, Time = DateTime.Now });
+        await base.OnDisconnectedAsync(exception);
     }
 
     public async Task SendMessage(string targetUserId, string message)
---
+++ e-Shop-Blogging/src/Volo.Blogging.Web/Pages/Blogs/Shared/Scripts/push.js
@@ -0,0 +1,28 @@
+﻿$(function() {
+    var connection = new signalR.HubConnectionBuilder().withUrl("/signalr-hubs/blogging").build();
+
+    connection.on("ReceiveMessage", function (message) {
+        $('#MessageList').append('<li><strong><i class="fas fa-long-arrow-alt-right"></i> ' + message + '</strong></li>');
+    });
+
+    connection.start().then(function (e) {
+        console.info("connection");
+
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    $(document).ready(function (e) {
+        var targetUserName = "lucy"; //$('#TargetUser').val();
+        var message = "hello"; //$('#Message').val();
+        $('#Message').val('');
+
+        connection.invoke("SendMessage", targetUserName, message)
+            .then(function () {
+                console.log('<li><i class="fas fa-long-arrow-alt-left"></i> ' + abp.currentUser.userName + ': ' + message + '</li>');
+            })
+            .catch(function(err) {
+                return console.error(err.toString());
+            });
+    });
+});
