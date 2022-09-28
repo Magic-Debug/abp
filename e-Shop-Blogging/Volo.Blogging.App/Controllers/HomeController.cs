62093ac91453c33d047c0eb36ee83d85667c5ce9
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2022-09-28 14:12:55 +0800

    ClusterIP

--- e-Shop-Blogging/Volo.Blogging.App/Controllers/HomeController.cs
+++ e-Shop-Blogging/Volo.Blogging.App/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Volo.Abp.AspNetCore.Mvc;
 using Volo.Blogging;
@@ -17,5 +21,42 @@ public class HomeController : AbpController
     {
         string urlPrefix = _blogOptions.RoutePrefix;
         return Redirect(urlPrefix);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("api/env")]
+    public ActionResult Env()
+    {
+        IDictionary envVariables = System.Environment.GetEnvironmentVariables();
+        return Json(envVariables);
+    }
+
+    [HttpGet("api/info")]
+    public ActionResult Info()
+    {
+        var env = new {
+            Environment.MachineName,
+            Environment.ProcessId,
+            Environment.OSVersion,
+            Environment.ProcessorCount,
+            Environment.SystemDirectory,
+            Environment.CurrentDirectory,
+            Environment.CommandLine,
+        };
+        return Json(env);
+    }
+
+    [HttpGet("api/host")]
+    public ActionResult Host()
+    {
+        string hostName = Dns.GetHostName();
+        IPHostEntry entry = Dns.GetHostEntry(hostName);
+        return Json(new {
+            HostName = entry.HostName,
+            Address = entry.AddressList.Select(address => address.ToString())
+        });
     }
 }
